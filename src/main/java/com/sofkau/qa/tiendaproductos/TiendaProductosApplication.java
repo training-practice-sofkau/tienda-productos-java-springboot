aa4f954761d137a9f26f28e3a3aa8b12f39ca636
Gretty Mosquera <gretymosquera@gmail.com>
2023-01-30 17:35:02 -0500

    Se realiza merge con la rama master

--- src/main/java/com/sofkau/qa/tiendaproductos/TiendaProductosApplication.java
+++ src/main/java/com/sofkau/qa/tiendaproductos/TiendaProductosApplication.java
@@ -8,6 +8,7 @@ public class TiendaProductosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaProductosApplication.class, args);
+		System.out.println("");
 	}
 
 }
