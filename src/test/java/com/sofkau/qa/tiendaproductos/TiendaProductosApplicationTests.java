cfef3aed927eb541ec8c350baafadfa10ca79e86
Santiago Posada <santiago90dell@gmail.com>
2023-01-30 11:57:21 -0500

    base project

---
+++ src/test/java/com/sofkau/qa/tiendaproductos/TiendaProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.qa.tiendaproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
