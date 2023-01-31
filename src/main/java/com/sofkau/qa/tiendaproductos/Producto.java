2f6030b0a08e371b50a580c9ddd04f3d007c0ddb
nunezjose770 <nunezjose770@gmail.com>
2023-01-31 00:05:38 -0500

    First Commit, app funcional

---
+++ src/main/java/com/sofkau/qa/tiendaproductos/Producto.java
@@ -0,0 +1,29 @@
+package com.sofkau.qa.tiendaproductos;
+
+public class Producto {
+    private String nombre;
+    private int precio;
+    private int stock;
+
+    public Producto(String nombre, int precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
